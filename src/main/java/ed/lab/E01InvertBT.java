47a35473c2bb81edafe1ea0d68885443e162c667
marcs <mwschaub@correo.url.edu.gt>
2025-03-24 16:57:28 -0600

    Ejercicio 1 test pasados

--- src/main/java/ed/lab/E01InvertBT.java
+++ src/main/java/ed/lab/E01InvertBT.java
@@ -1,8 +1,14 @@
 package ed.lab;
-
 public class E01InvertBT {
     public TreeNode<Integer> invertTree(TreeNode<Integer> root) {
-        return root;
+
+        if (root == null) return null; // Caso base: si el nodo es nulo, retornamos null
+
+        // Intercambiamos los hijos izquierdo y derecho
+        TreeNode<Integer> temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
+        return root; // Retornamos la nueva raíz invertida
     }
-    //hola hola
 }
